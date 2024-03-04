70e6ac8f34fa4299f1e5f6383a1c15f454dc283f
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-03-04 20:12:42 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// 100 Days Challange (Governer Initiative)
+//Question No: 2
+var personName = "Arsalan";
+console.log("Hello ".concat(personName, ", would you like to learn some TypeScript today?"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+// 100 Days Challange (Governer Initiative)
+
+//Question No: 2
+let personName : string = "Arsalan"
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`)
