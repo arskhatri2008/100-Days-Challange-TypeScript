f7720a442a4f8406511b7ad9fa9bc5c95e1f03ca
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-03-04 21:18:39 +0500

    continue

--- main.js
+++ main.js
@@ -2,3 +2,26 @@
 //Question No: 2
 var personName = "Arsalan";
 console.log("Hello ".concat(personName, ", would you like to learn some TypeScript today?"));
+//Question No: 3
+var personName_1 = "arsalan";
+console.log("To UpperCase: ".concat(personName_1.toUpperCase()));
+console.log("To LowerCase: ".concat(personName_1.toLowerCase()));
+console.log("To TitleCase: ".concat(personName_1.charAt(0).toUpperCase() + personName_1.slice(1).toLowerCase()));
+//Question No: 4
+console.log("Albert Einstein once said, \"A person who never made a mistake never tried anything new.\"");
+//Question No: 5
+var famous_person = "Albert Einstein";
+var message = "A person who never made a mistake never tried anything new.";
+console.log("".concat(famous_person, " once said, \"").concat(message, "\""));
+//Question No: 6
+var trim_name = "\t\nArsalan\t\n";
+console.log(trim_name);
+console.log(trim_name.trim());
+//Question No: 7 & 8
+console.log(5 + 3); //Addition
+console.log(10 - 2); //Subtraction
+console.log(2 * 4); //Multiplication
+console.log(16 / 2); //Division
+// Question No: 9
+var favoriteNumber = 7;
+console.log("My favorite number is ".concat(favoriteNumber, "."));
--- main.ts
+++ main.ts
@@ -3,3 +3,32 @@
 //Question No: 2
 let personName : string = "Arsalan"
 console.log(`Hello ${personName}, would you like to learn some TypeScript today?`)
+
+//Question No: 3
+let personName_1: string = "arsalan"
+console.log(`To UpperCase: ${personName_1.toUpperCase()}`)
+console.log(`To LowerCase: ${personName_1.toLowerCase()}`)
+console.log(`To TitleCase: ${personName_1.charAt(0).toUpperCase()+personName_1.slice(1).toLowerCase()}`)
+
+//Question No: 4
+console.log(`Albert Einstein once said, "A person who never made a mistake never tried anything new."`);
+
+//Question No: 5
+let famous_person : string = "Albert Einstein"
+let message : string = "A person who never made a mistake never tried anything new."
+console.log(`${famous_person} once said, "${message}"`)
+
+//Question No: 6
+let trim_name = "\t\nArsalan\t\n"
+console.log(trim_name);
+console.log(trim_name.trim());
+
+//Question No: 7 & 8
+console.log(5+3); //Addition
+console.log(10-2); //Subtraction
+console.log(2*4); //Multiplication
+console.log(16/2); //Division
+
+// Question No: 9
+let favoriteNumber : number = 7
+console.log(`My favorite number is ${favoriteNumber}.`)
